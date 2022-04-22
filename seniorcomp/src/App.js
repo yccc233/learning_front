eab02c73d4355a984079315f41eab1db23c559ec
yccc233 <1099312164@qq.com>
2022-04-23 00:36:29 +0800

    高阶组件-office文件在线预览，还有cors未解决

--- seniorcomp/src/App.js
+++ seniorcomp/src/App.js
@@ -1,27 +1,31 @@
-import './App.css';
+import './css/App.css';
 import {Route, Link, BrowserRouter, Redirect} from "react-router-dom";
 import OfflineMap from "./map/offlinemap";
-
+import OfficeView from "./officeview/officeview";
 
 function App() {
-  return (
-      <div style={{width: "100%"}}>
-        {/*这里Switch 和 exact 用一个就够了*/}
-        <BrowserRouter basename={"/"}>
-            <Route exact path={"/"} render={() => <Redirect to={"/home"} />} />
-            <Route path={"/home"} component={Home} />
-            <Route path={"/offlinemap"} component={OfflineMap} />
-        </BrowserRouter>
-      </div>
-  );
+    return (
+        <div style={{width: "100%"}}>
+            {/*这里Switch 和 exact 用一个就够了*/}
+            <BrowserRouter basename={"/"}>
+                <Route exact path={"/"} render={() => <Redirect to={"/home"} />} />
+                <Route path={"/home"} component={Home} />
+                <Route path={"/offlinemap"} component={OfflineMap} />
+                <Route path={"/officeview"} component={OfficeView} />
+            </BrowserRouter>
+        </div>
+    );
 }
 
 const Home = () => {
-  return <div style={{textAlign: "center", margin: "50px 0"}}>
-    <h2>离线地图</h2>
-    <Link to={"/offlinemap"}>Go</Link>
-    <Block />
-  </div>
+    return <div style={{textAlign: "center", margin: "50px 0"}}>
+        <h2>网页预览office</h2>
+        <Link to={"/officeview"}>Go</Link>
+        <Block />
+        <h2>离线地图</h2>
+        <Link to={"/offlinemap"}>Go</Link>
+        <Block />
+    </div>
 }
 
 const Block = () => <div style={{width: "100%", height: "50px"}}/>
---
+++ seniorcomp/src/officeview/officeview.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import "./office.css";
+import {Select, Input, Button, Form, Modal} from "antd";
+import FileViewer from "react-file-viewer-plugins-v3";
+
+const {Component, Fragment, createRef} = React;
+const {Option} = Select;
+const {Item} = Form;
+
+export default class OfficeView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSee: false,
+            files: ['pdf', 'doc', 'docx']
+        };
+        this.formRef = createRef();
+    }
+
+    inputChange = e => {
+        let v = e.target.value;
+        let fileType = v.split('.')[v.split('.').length - 1];
+        this.state.files.includes(fileType) && this.formRef.current.setFieldsValue({fileType});
+    }
+
+    render() {
+        return <Fragment>
+            <div>
+                <Form ref={this.formRef}>
+                    <Item name={["fileSource"]} initialValue={"http://ycc.com/files/"}>
+                        <Input placeholder={"文件路径"} onChange={this.inputChange} style={{width: 500}}/>
+                    </Item>
+                    <Item name={["fileType"]}>
+                        <Select placeholder={"选择文件类型！"} onChange={v => this.setState({fileType: v})} style={{width: 400}}>
+                            {this.state.files.map(ft => (
+                                <Option value={ft} key={ft}>{ft}</Option>
+                            ))}
+                        </Select>
+                    </Item>
+                </Form>
+                {/*<Button type={"primary"} onClick={() => console.log(this.state)}>See</Button>*/}
+                <Button type={"primary"} onClick={() => this.setState({isSee: true})}>See</Button>
+            </div>
+            {
+                this.state.isSee
+                && <FileView
+                    isvisible={this.state.isSee}
+                    fileType={this.formRef.current.getFieldsValue().fileType}
+                    fileSource={this.formRef.current.getFieldsValue().fileSource}
+                    onCancel={() => this.setState({isSee: false})}
+                />
+            }
+        </Fragment>
+    }
+}
+
+
+class FileView extends Component {
+    constructor(props) {
+        super(props);
+        this.state={};
+        console.log("文件参数", props)
+    }
+
+    render() {
+        const {isvisible, fileType, fileSource, onCancel} = this.props;
+        return (
+            <Modal
+                visible={isvisible}
+                title={`${fileType}文件`}
+                footer={null}
+                onCancel={onCancel}
+                width={"80%"}
+            >
+               {/* 对于pdf有一个默认的工具 */}
+               {/*<iframe src={fileSource} style={{width: "100%", height: 600}}></iframe>*/}
+                <FileViewer
+                    crossOrigin={"anonymous"}
+                    fileType={fileType}
+                    filePath={fileSource}
+                    errorComponent={<h1>有些小问题</h1>}
+                    onError={err => console.log("something is wrong：", err)}/>
+            </Modal>
+        );
+    }
+}
