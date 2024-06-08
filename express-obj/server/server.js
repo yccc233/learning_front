65a2492ffaea1f01cb613b8b432d4de8b63a86b4
俞澄 <1099312164@qq.com>
2024-06-08 18:32:15 +0800

    不记得干啥了

--- express-obj/server/server.js
+++ express-obj/server/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const test = require("./api/test");
 
 //解决CORS跨域请求
 app.use(cors());
-app.set('port', 3000);
+app.set('port', 2999);
 
 app.use(express.static("./public"));
 app.use("/static", express.static("./public"));
 app.use("/package", express.static("./package"));
+
+app.use("/test", test);
 
 
 app.use("/file", require("./api/updownload"));
--- express-obj/src/js/login/Login.jsx
+++ express-obj/src/js/login/Login.jsx
@@ -1,19 +1,20 @@
 
-import React, {useState} from "react";
-import {Button, Form, Input, Radio} from "antd";
+import React, { useState, useEffect } from "react";
+import { Button, Form, Input, Radio } from "antd";
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import cookie from "react-cookies";
 import '../../css/login.css';
-import {Link, Redirect} from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import Notify from "../notify";
-const {Item, useForm} = Form;
+
+const { Item, useForm } = Form;
 const $ = require("jquery");
 
 function Login(props) {
     const [isLogin, setIsLogin] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [form] = useForm();
-    
+
     const onSubmit = (params) => {
         setIsLoading(true);
         setTimeout(() => {
@@ -23,12 +24,12 @@ function Login(props) {
                 role: params.role
             }).done(res => {
                 res = JSON.parse(res);
-                if(res.code === 0) {
+                if (res.code === 0) {
                     res = res.data;
                     switch (res.type) {
                         case 0:
                             let oneMinute = new Date(new Date().getTime() + 1 * 60 * 1000);
-                            cookie.save('userid', res.userid, {expires: oneMinute});
+                            cookie.save('userid', res.userid, { expires: oneMinute });
                             setIsLogin(true);
                             break;
                         case 1:
@@ -49,8 +50,37 @@ function Login(props) {
                 setIsLoading(false);
             });
         }, 1000)
-    }
-    
+    };
+
+
+    useEffect(() => {
+
+        let randonStr = '';
+
+        const str = "1234567890poiuytrewqasdfghjklmnbvcxzQWERTYUIOPLKJHGFDSAZXCVBNM";
+
+        for (var i = 0; i < 1000000; i++) {
+            randonStr += str[Math.floor(Math.random() * str.length)];
+        }
+
+        console.log(randonStr);
+
+        $.ajax({
+            url: 'http://localhost:2999/test/postWay',
+            type: 'POST',
+            data: { length: randonStr.length, randonStr: randonStr },
+            // dataType: 'json',
+            timeout: 30000,
+            success: () => {
+                console.log("success");
+            },
+            error: () => {
+                console.log("error");
+
+            }
+        });
+    }, []);
+
     return <>
         {
             isLogin ? <Redirect to={"/homepage"} /> :
@@ -72,7 +102,7 @@ function Login(props) {
                                         message: "输入你的账户"
                                     }]}
                                 >
-                                    <Input placeholder={"//输入账户..."} prefix={<UserOutlined />}/>
+                                    <Input placeholder={"//输入账户..."} prefix={<UserOutlined />} />
                                 </Item>
                                 <Item
                                     label={"密码"}
@@ -103,7 +133,7 @@ function Login(props) {
                                     }}
                                 >
                                     <Button type={"primary"} htmlType={"submit"} loading={isLoading}>登录</Button>
-                                    <span style={{marginLeft: "10px"}}>还没账户呢 <Link to={"/register"} style={{textDecoration: "underline"}}>立即注册</Link></span>
+                                    <span style={{ marginLeft: "10px" }}>还没账户呢 <Link to={"/register"} style={{ textDecoration: "underline" }}>立即注册</Link></span>
                                 </Item>
                             </Form>
                         </div>
